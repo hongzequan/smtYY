b8c244959325c0e8e9633768c56a11231ca671ae
hongzequan <hongzequan@hotmail.com>
2018-05-10 11:50:00 +0800

    第一个版本 基础框架加UI框架

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="wrap">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+
+</style>
